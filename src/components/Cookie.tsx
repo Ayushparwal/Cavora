ef69e00fa8e9b57e201ee418e9ab56b26e3f4944
Ayushparwal <parwalayush5@gmail.com>
2025-06-13 11:20:23 +0530

    main

--- src/components/Cookie.tsx
+++ src/components/Cookie.tsx
@@ -92,12 +92,7 @@ const Cookies = () => {
             >
               View Privacy Policy
             </Link>
-            <Link
-              to="/signup"
-              className="px-6 py-3 bg-gradient-to-r from-indigo-600 to-cyan-500 text-white rounded-xl hover:from-indigo-700 hover:to-cyan-600 transition-all"
-            >
-              Back to Sign Up
-            </Link>
+            
           </div>
         </motion.div>
       </div>
